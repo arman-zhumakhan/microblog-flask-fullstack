c8f280d80e7fe4d64fdcd9a34ecf53061a357c73
Arman Zhumakhan <arman310501@gmail.com>
2025-08-07 13:03:41 +0800

    Add Token authentication and protect API endpoints

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,10 +1,12 @@
 import sqlalchemy as sa
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+
 from app import db
-from app.models import User
 from app.api.errors import error_response
+from app.models import User
 
 basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
 
 @basic_auth.verify_password
@@ -13,6 +15,17 @@ def verify_password(username, password):
     if user and user.check_password(password):
         return user
 
+
 @basic_auth.error_handler
 def basic_auth_error(status):
+    return error_response(status)
+
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+
+@token_auth.error_handler
+def token_auth_error(status):
     return error_response(status)
